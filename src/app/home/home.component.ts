19c7b7b0802fb9db9e9d3851cd9d50b557d68bdf
Andrejp90 <puskasandrej@hotmail.com>
2024-01-16 19:49:01 +0100

    Add forkjoin to home.component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,20 @@
-import { Component } from "@angular/core";
-import { NameInfoService } from "../services/name-info.service";
-import { Gender } from "../interfaces/gender";
-import { Age } from "../interfaces/age";
-import { CountryFinal } from "../interfaces/country";
+import { Component } from '@angular/core';
+import { NameInfoService } from '../services/name-info.service';
+import { Gender } from '../interfaces/gender';
+import { Age } from '../interfaces/age';
+import { CountryFinal } from '../interfaces/country';
+import { forkJoin } from 'rxjs';
 
-
-
-@Component ({
-    selector: 'app-home',
+@Component({
+  selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-
-
 export class HomeComponent {
-    labelValue: string = 'Unesite ime:';
+  labelValue: string = 'Unesite ime:';
   name: string = '';
-  gender: Gender | null = null ;
-  age: Age | null = null ;
+  gender: Gender | null = null;
+  age: Age | null = null;
   country: CountryFinal | null = null;
   isLoading: boolean = false;
 
@@ -26,24 +23,15 @@ export class HomeComponent {
   onButtonClick() {
     this.isLoading = true;
 
-    setTimeout(() => {
-
-    this.nameInfoService.getGenderByName(this.name).subscribe(gender=>{
-      this.gender= gender});
-
-    this.nameInfoService.getAgeByName(this.name).subscribe(age=>{
-      this.age= age});
-
-    this.nameInfoService.getCountryByName(this.name).subscribe(country=>{
-      this.country = country;
-
+    forkJoin([
+      this.nameInfoService.getGenderByName(this.name),
+      this.nameInfoService.getAgeByName(this.name),
+      this.nameInfoService.getCountryByName(this.name),
+    ]).subscribe((value: [Gender, Age, CountryFinal ]) => {
+      this.age = value[1];
+      this.country = value[2];
+      this.gender = value[0];
       this.isLoading = false;
     });
-  }, 3000);
+  }
 }
-}
-    
-
-
-
-
